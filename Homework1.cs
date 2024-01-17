7e062ca03bdc93a283158ccb59144632e90628d7
Emilyfay51 <156862462+Emilyfay51@users.noreply.github.com>
2024-01-17 17:39:21 -0600

    Homework1 code file

---
+++ Homework1.cs
@@ -0,0 +1,19 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    // The code for Homework1
+    double X=2.5;
+    double Y=3.3;
+    double Z=4 * X* X + 3 * Y;
+    // Display value of Z
+    Console.WriteLine("X=2.5,Y=3.3");
+    Console.WriteLine("The Value of Z is:" + Z);
+
+        
+        
+
+    }
+}
